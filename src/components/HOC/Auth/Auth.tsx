0fff102d31f1ac33f7a35ca3fb19fd275ed7d9c1
강성훈 <yolly050101@naver.com>
2023-05-19 09:56:28 +0900

    auth

--- src/components/HOC/Auth/Auth.tsx
+++ src/components/HOC/Auth/Auth.tsx
@@ -1,16 +1,18 @@
 import React, { PropsWithChildren, useEffect } from "react";
+import { useLocation } from "react-router";
 import { useRecoilState } from "recoil";
 import { loginState } from "../../../atom/login/Login";
 import LoginPage from "../../../pages/LoginPage";
 
 const Auth = ({ children }: PropsWithChildren<unknown>) => {
   const [auth, setAuth] = useRecoilState(loginState);
+  const location = useLocation();
 
   useEffect(() => {
     if (localStorage.getItem("Authorization")) {
       setAuth(true);
     }
-  }, []);
+  }, [location]);
 
   if (!auth) {
     return <LoginPage />;
